2668494f8bf9fb15bd0ff35975c4f1814dda3069
nikhil <nikhil.badani@gmail.com>
2018-03-04 00:44:39 +0530

    about page

---
+++ about.php
@@ -0,0 +1,68 @@
+<html>
+<head>
+	<script src="jquery.min.js"></script>
+	<script src="bootstrap.min.js"></script>
+	<link href="css files/bootstrap.min.css" rel="stylesheet">
+	<link href="css files/aboutstyle.css" rel="stylesheet">
+</head>
+<body>
+	<div id="main">
+		<div id="header">
+			<div class="container">
+				<nav class="navbar navbar-inverse">
+					<div class="container-fluid">
+						<div class="navbar-header">
+							<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>                        
+							</button>
+						</div>
+						<div class="collapse navbar-collapse" id="myNavbar">
+							<ul class="nav navbar-nav">
+								<li class="dropdown">
+									<a class="dropdown-toggle" data-toggle="dropdown" href="#"><b>HELP</b> <span class="caret"></span></a>
+									<ul class="dropdown-menu">
+										<li><a href="about.php">About</a></li>
+										<li><a href="create.php">How To Create a Gmail Account</a></li>
+									</ul>
+								</li>
+							</ul>
+							<ul class="nav navbar-nav navbar-right">
+								<li>
+									<a href="register.php"><span class="glyphicon glyphicon-user"></span> Sign Up</a>
+								</li>
+								<li>
+									<a href="modal.php"><span class="glyphicon glyphicon-log-in"></span> Login</a>
+								</li>
+							</ul>
+						</div>
+					</div>
+				</nav>
+			</div>
+		</div>
+		<div id="footer">
+			<img src="img1.png" id="i1">
+			<img src="img2.png" id="i2">
+			<img src="img3.png" id="i3">
+			<img src="img4.png" id="i4">
+			<img src="img5.png" id="i5">
+			<img src="img6.png" id="i6">
+			<img src="img7.png" id="i7">
+			<table id="ul">
+				<tr>
+					<td>#<b>Gmail is a free,advertising-supported email service developed by Google</b>.</td>
+				</tr>
+				<tr>
+					<td>#<b>Users can access Gmail on the web and through  the mobile apps for Android and iOS,<br>as well as through third-party programs that synchronise email content  through POP or<br> IMAP protocols.</b></td>
+				</tr>
+				<tr>
+					<td>#<b>Google's mail servers automatically scan emails for multiple purpose,including <br>to add context-sensitive advertisements next to emails,and to filter spam and<br> malware.</b></td>
+				</tr>
+				<tr>
+					<td>#<b>Google has stated that "all users of email must necessarily expect that their emails<br> will be subject to automated processing[...]".</b></td>
+				</tr>
+			</table>
+		</div>
+	</div>
+</body>
