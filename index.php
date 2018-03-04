849dbb7a30b46e46ac5ea87f7b22e2dc5b9f535f
nikhil <nikhil.badani@gmail.com>
2018-03-04 22:19:19 +0530

    properlocation for images added

--- index.php
+++ index.php
@@ -10,20 +10,20 @@ if(isset($_REQUEST["b1"])){
 <link href="css files/indexstyle.css" rel="stylesheet">
 <div id="main">
 	<div id="header">
-		<img src="j1.png" id="cloud">
-		<img src="j1.png" id="cloud1">
+		<img src="images/j1.png" id="cloud">
+		<img src="images/j1.png" id="cloud1">
 		<p id="wel"><b>WELCOME TO GMAIL</b></p>
 	</div>
 	<div id="footer">
-		<img src="newdone.png" id="img">
-		<img src="j1.png" id="cloud2">
-		<img src="j1.png" id="cloud3">
+		<img src="images/newdone.png" id="img">
+		<img src="images/j1.png" id="cloud2">
+		<img src="images/j1.png" id="cloud3">
 	</div>
 </div>
 <form>
 <input type="submit" value="welcome to gmail" name="b1" id="b">
 </form>
-<script src="jq.js"></script>
+<script src="javascript files/jq.js"></script>
 <script>
 $(document).ready(function(){
 	setInterval(function(){
