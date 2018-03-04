60ca3ee90cb9279d2feb2f912d853f40cd93e619
nikhil <nikhil.badani@gmail.com>
2018-03-05 01:05:17 +0530

    finaaly menu done

--- menu.php
+++ menu.php
@@ -1,57 +1,63 @@
 <html>
 <head>
-<script src="javascript files/jquery.min.js"></script>
-<script src="javascript files/bootstrap.min.js"></script>
-<link href="css files/bootstrap.min.css" rel="stylesheet">
-<link href="css files/menustyle.css" rel="stylesheet">
+<script src="javascriptfiles/jquery.min.js"></script>
+<script src="javascriptfiles/bootstrap.min.js"></script>
+<link href="cssfiles/bootstrap.min.css" rel="stylesheet">
+<link href="cssfiles/menustyle.css" rel="stylesheet">
 </head>
 <body>
 <script>
 $(document).ready(function(){
 	setInterval(function(){
 	$('#bl').animate({
-top:'200px'},4000).animate({left:'860px'},4000).animate({top:'260px'},4000).animate({top:'200px'},4000).animate({left:'370px'},4000).animate({top:'260px'},4000);
+top:'200px'},4000).animate({left:'1100px'},4000).animate({top:'430px'},4000).animate({top:'200px'},4000).animate({left:'420px'},4000).animate({top:'460px'},4000);
 	},1000);
 });
 </script>
-<div class="container">
-	<img src="images/boyy.jpg" id="bo">
-	<img src="images/girll.png" id="go">
-	<img src="images/black.png" id="bl">
-	<nav class="navbar navbar-inverse">
-		<div class="container-fluid">
-			<div class="navbar-header">
-				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>                        
-				</button>
+<div id="main1">
+	<div class="container">
+		<img src="images/finalboy.png" id="bo">
+		<img src="images/finalgirl.png" id="go">
+		<img src="images/finalblack.png" id="bl">
+		<img src="images/j1.png" id="cloud">
+		<img src="images/j1.png" id="cloud2">
+		<img src="images/j1.png" id="cloud3">
+		<img src="images/j1.png" id="cloud1">
+		<nav class="navbar navbar-default">
+			<div class="container-fluid">
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>                        
+					</button>
+				</div>
+				<div class="collapse navbar-collapse" id="myNavbar">
+					<ul class="nav navbar-nav">
+						<li class="dropdown">
+							<a class="dropdown-toggle" data-toggle="dropdown" href="#"><b>HELP</b> 
+								<span class="caret"></span>
+							</a>
+							<ul class="dropdown-menu">
+								<li>
+									<a href="about.php">About</a>
+								</li>
+								<li>
+									<a href="create.php">How To Create a Gmail Account</a>
+								</li>
+							</ul>
+						</li>
+					</ul>
+					<ul class="nav navbar-nav navbar-right">
+						<li>
+							<a href="registermodal.php"><span class="glyphicon glyphicon-user"></span> Sign Up</a>
+						</li>
+						<li>
+						<a href="modal.php"><span class="glyphicon glyphicon-log-in"></span> Login</a>
+						</li>
+					</ul>
+				</div>
 			</div>
-			<div class="collapse navbar-collapse" id="myNavbar">
-				<ul class="nav navbar-nav">
-					<li class="dropdown">
-						<a class="dropdown-toggle" data-toggle="dropdown" href="#"><b>HELP</b> 
-							<span class="caret"></span>
-						</a>
-						<ul class="dropdown-menu">
-							<li>
-								<a href="about.php">About</a>
-							</li>
-							<li>
-								<a href="create.php">How To Create a Gmail Account</a>
-							</li>
-						</ul>
-					</li>
-				</ul>
-				<ul class="nav navbar-nav navbar-right">
-					<li>
-						<a href="registermodal.php"><span class="glyphicon glyphicon-user"></span> Sign Up</a>
-					</li>
-					<li>
-					<a href="modal.php"><span class="glyphicon glyphicon-log-in"></span> Login</a>
-					</li>
-				</ul>
-			</div>
-		</div>
-	</nav>
+		</nav>
+	</div>
 </div>
