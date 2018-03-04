d4a746d8b49970dc812c218d537a41de1a295e3e
nikhil <nikhil.badani@gmail.com>
2018-03-05 02:01:35 +0530

    id added to nav fields

--- about.php
+++ about.php
@@ -1,15 +1,15 @@
 <html>
 <head>
-	<script src="jquery.min.js"></script>
-	<script src="bootstrap.min.js"></script>
-	<link href="css files/bootstrap.min.css" rel="stylesheet">
-	<link href="css files/aboutstyle.css" rel="stylesheet">
+	<script src="javascriptfiles/jquery.min.js"></script>
+	<script src="javascriptfiles/bootstrap.min.js"></script>
+	<link href="cssfiles/bootstrap.min.css" rel="stylesheet">
+	<link href="cssfiles/styabout.css" rel="stylesheet">
 </head>
 <body>
 	<div id="main">
 		<div id="header">
 			<div class="container">
-				<nav class="navbar navbar-inverse">
+				<nav class="navbar navbar-default" id="nav">
 					<div class="container-fluid">
 						<div class="navbar-header">
 							<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
@@ -21,19 +21,19 @@
 						<div class="collapse navbar-collapse" id="myNavbar">
 							<ul class="nav navbar-nav">
 								<li class="dropdown">
-									<a class="dropdown-toggle" data-toggle="dropdown" href="#"><b>HELP</b> <span class="caret"></span></a>
+									<a class="dropdown-toggle" data-toggle="dropdown" href="#" id="help"><b>HELP</b> <span class="caret"></span></a>
 									<ul class="dropdown-menu">
-										<li><a href="about.php">About</a></li>
-										<li><a href="create.php">How To Create a Gmail Account</a></li>
+										
+										<li><a href="create.php" id="how">How To Create a Gmail Account</a></li>
 									</ul>
 								</li>
 							</ul>
 							<ul class="nav navbar-nav navbar-right">
 								<li>
-									<a href="register.php"><span class="glyphicon glyphicon-user"></span> Sign Up</a>
+									<a href="register.php"><span class="glyphicon glyphicon-user" id="sign"></span> Sign Up</a>
 								</li>
 								<li>
-									<a href="modal.php"><span class="glyphicon glyphicon-log-in"></span> Login</a>
+									<a href="modal.php"><span class="glyphicon glyphicon-log-in" id="log"></span> Login</a>
 								</li>
 							</ul>
 						</div>
@@ -42,25 +42,22 @@
 			</div>
 		</div>
 		<div id="footer">
-			<img src="img1.png" id="i1">
-			<img src="img2.png" id="i2">
-			<img src="img3.png" id="i3">
-			<img src="img4.png" id="i4">
-			<img src="img5.png" id="i5">
-			<img src="img6.png" id="i6">
-			<img src="img7.png" id="i7">
+		
 			<table id="ul">
 				<tr>
-					<td>#<b>Gmail is a free,advertising-supported email service developed by Google</b>.</td>
+					<td id="head"><h1><b>YOU  MUST  KNOW  ABOUT THIS  </b></h1></td>
 				</tr>
 				<tr>
-					<td>#<b>Users can access Gmail on the web and through  the mobile apps for Android and iOS,<br>as well as through third-party programs that synchronise email content  through POP or<br> IMAP protocols.</b></td>
+					<td>1. <b>Gmail is a free,advertising-supported email service developed by Google</b>.</td>
 				</tr>
 				<tr>
-					<td>#<b>Google's mail servers automatically scan emails for multiple purpose,including <br>to add context-sensitive advertisements next to emails,and to filter spam and<br> malware.</b></td>
+					<td>2. <b>Users can access Gmail on the web and through  the mobile apps for Android <br/>and iOS,as well as through third-party programs that synchronise email content <br> through POP or IMAP protocols.</b></td>
 				</tr>
 				<tr>
-					<td>#<b>Google has stated that "all users of email must necessarily expect that their emails<br> will be subject to automated processing[...]".</b></td>
+					<td>3. <b>Google's mail servers automatically scan emails for multiple purpose,including <br>to add context-sensitive advertisements next to emails,and to filter spam and<br> malware.</b></td>
+				</tr>
+				<tr>
+					<td>4. <b>Google has stated that "all users of email must necessarily expect that their <br>emails will be subject to automated processing[...]".</b></td>
 				</tr>
 			</table>
 		</div>
