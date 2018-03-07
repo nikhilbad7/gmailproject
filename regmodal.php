d2e1d864eccbdd5785c7a7a13017555681b88de0
nikhil <nikhil.badani@gmail.com>
2018-03-08 05:11:36 +0530

    registration done

---
+++ regmodal.php
@@ -0,0 +1,78 @@
+<?php
+// define variables and set to empty values
+include 'db.php';
+$passErr = $emailErr = $mobErr = $addressErr ="";
+$email = $pass = $mob = $add ="";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  
+  if (empty($_POST["password"])) {
+    $passErr = "Password is required";
+  } else {
+    $pass = test_input($_POST["password"]);
+    // check if password only contains some letters and whitespace
+    if (!preg_match('/^(?=.*\d)(?=.*[A-Za-z])[0-9A-Za-z!@#$%]{8,50}$/', $pass)) {
+      $passErr = "The password must contains 1number,and 1 specialcharacter"; 
+    }
+  }
+  
+  if (empty($_POST["email"])) {
+    $emailErr = "Email is required";
+  } else {
+    $email = test_input($_POST["email"]);
+    // check if e-mail address is well-formed
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+      $emailErr = "Invalid email format"; 
+    }
+  }
+    
+  if (empty($_POST["mob"])) {
+    $mobErr = " please fill this field";
+  } else {
+    $mob = test_input($_POST["mob"]);
+    // check if mobileno is valid
+    if (!preg_match("/^(\+\d{1,3}[- ]?)?\d{10}$/",$mob)) {
+      $mobErr = "Invalid mobileno"; 
+    }    
+  }
+  
+  if (empty($_POST["add"])) {
+    $addressErr = " please fill this field";
+  }
+
+
+
+
+	$add=$_REQUEST['add'];
+	$image=$_FILES['f']['name'];
+	$tmp=$_FILES['f']['tmp_name']; 
+	 if(move_uploaded_file($tmp,$image))
+    {
+	$q="insert into registration  values('$email','$pass','$mob','$add','$image')";
+	$rs=$con->query($q);
+	if($rs)
+	{
+		echo"<script>
+			window.location='registrationsuccessfull.php';
+			</script>
+		";
+	}
+	else
+	{
+			echo "Error while registering";
+	}
+
+
+}
+else{
+	echo "error while uploading image";
+}}
+
+function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
+
+	?> 
